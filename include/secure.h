846d3f124020cb4488b9d755261fb0e565f36def
Felipe <felipedias2508@gmail.com>
2025-12-27 23:29:12 -0300

    gitIgnore updated

--- include/secure.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace Secure {
-    const char* SSID = "Neto_2.4Ghz";
-    const char* PASSWORD = "36251469JN";
-    const char* BOT_TOKEN = "8543802885:AAHRpnUVSAfMlf6VZ4AgXTM8f9NoSPQzHpI";
-}
-
